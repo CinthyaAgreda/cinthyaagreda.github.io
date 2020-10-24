f92051a59cfdd3ac10f24e3ce024fdd599608d65
CinthyaAgreda <cinthya6@byui.edu>
2020-10-24 00:24:24 -0500

    footwear

---
+++ cit160/js/footwear.js
@@ -0,0 +1,26 @@
+function footware() {
+    //Input weather from the user and define variable shoes
+    let weather = document.getElementById("weather-select").value
+    let shoes;
+
+    //Processing change the variable shoes depending on the weather the user picks
+    if (weather = "") {
+        shoes = "--Please Choose an Option--";
+    }
+    else if (weather = "hot") {
+        shoes = "sandals";
+    }
+    else if (weather = "rain") {
+        shoes = "galoshes";
+    }
+    else if (weather = "snow") {
+        shoes = "boots";
+    }
+    else if (weather === "unknown") {
+        shoes = "shoes";
+    }
+
+    //Output what shoes to wear depending on the weather the user picks
+document.getElementById("output").innerHTML = "When the weather is " + weather + " you should wear " + shoes + "."
+
+}
