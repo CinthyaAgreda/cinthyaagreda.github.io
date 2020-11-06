d16f20d1980ef46ea331cfdfcb9622f12fa0a7b1
CinthyaAgreda <cinthya6@byui.edu>
2020-11-05 23:10:27 -0500

    alarm update

---
+++ cit160/js/alarm.js
@@ -0,0 +1,23 @@
+function alarm() {
+//Input
+let now = new Date();
+let month = now.getMonth();
+let dayOfMonth = now.getDate();
+let dayOfWeek = now.getDay();
+let message;
+
+// Processing
+if ((month == 0 && dayOfMonth == 1) || (month == 6 && dayOfMonth == 4) || 
+(month == 11 && dayOfMonth == 25) || (dayOfWeek === 0) || (dayOfWeek === 6)) {
+
+    message = "Sleep In";
+}
+
+else {
+
+    message = "Get Up!";
+}
+
+// Output
+document.getElementById('output').innerHTML = message
+}
