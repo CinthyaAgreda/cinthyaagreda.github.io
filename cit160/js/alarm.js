5b5e1b025ea879deff7fceed2d40e0ead78e2518
CinthyaAgreda <cinthya6@byui.edu>
2020-11-07 00:45:29 -0500

    up

--- cit160/js/alarm.js
+++ cit160/js/alarm.js
@@ -10,12 +10,12 @@ let message;
 if ((month == 0 && dayOfMonth == 1) || (month == 6 && dayOfMonth == 4) || 
 (month == 11 && dayOfMonth == 25) || (dayOfWeek === 0) || (dayOfWeek === 6)) {
 
-    message = "Sleep In";
+    message = "Keep Sleeping";
 }
 
 else {
 
-    message = "Get Up!";
+    message = "Wake Up";
 }
 
 // Output
