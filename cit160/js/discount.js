45f2e470afa7f1abfa93c56ea4f14c1d7590ca8b
CinthyaAgreda <cinthya6@byui.edu>
2020-11-06 23:28:34 -0500

    update

--- cit160/js/discount.js
+++ cit160/js/discount.js
@@ -1,18 +1,19 @@
 function sales() {
-
     //Input
-        let dayOfWeek = new Date().getDay();
-        let subtotal = parseFloat(document.getElementById("subtotal").value);
+    let dayOfWeek = new Date().getDay();
+    let subtotal = parseFloat(document.getElementById("subtotal").value);
 
-    // Processing
-    
-        if (subtotal > 50 && (dayOfWeek == 2 || dayOfWeek == 3)) {
-            discount = subtotal - (subtotal * .10);
+    //Processing
+
+    if (subtotal <50 && (dayOfWeek == 2 || dayOfWeek == 3)){
+        discount = subtotal - (subtotal * .10)
+
     }
-        else {
+
+    else {
         discount = subtotal;
     }
-    //Output
-    
-        document.getElementById('output').innerHTML = "You Owe: $" + (discount + (discount * .06)).toFixed(2)
+     //Output
+
+     document.getElementById('output').innerHTML = "Pay: $" + ((discount) + (discount * .06)).toFixed(2)
 }
